74bb16895425fcb9b60476488810ed38fb47b83f
Igor Shuliakov <igor.shuliakov@nav.no>
2023-08-10 10:47:01 +0200

    React 18 render

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import Mikrofrontend from "./Mikrofrontend";
 import "./index.css";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(
   <React.StrictMode>
     <main>
       <Mikrofrontend />
     </main>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
