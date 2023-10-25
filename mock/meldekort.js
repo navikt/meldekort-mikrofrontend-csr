781647e1deb6c81a82aaa490ea4a8f3fc5e395cb
Igor Shuliakov <igor.shuliakov@nav.no>
2023-10-25 14:05:32 +0200

    Code beautifying for MeldekortDataFraApi + endre mock data iht type MeldekortDataFraApi

--- mock/meldekort.js
+++ mock/meldekort.js
@@ -4,16 +4,13 @@ export default [
     method: "get",
     response: () => {
       return {
+        antallGjenstaaendeFeriedager: 0,
         etterregistrerteMeldekort: 2,
-        meldekortbruker: true,
-        nesteInnsendingAvMeldekort: "2019-09-30",
         meldekort: 2,
-        resterendeFeriedager: 2,
+        nesteInnsendingAvMeldekort: "2019-09-30",
         nesteMeldekort: {
           fra: "2019-09-09",
           kanSendesFra: "2019-09-21",
-          risikererTrekk: false,
-          sisteDatoForTrekk: "2019-09-30",
           til: "2024-09-22",
           uke: "37-38",
         },
